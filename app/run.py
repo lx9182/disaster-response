542ada1f5d23d8d60b354c1dd75f0fd3aeb84f81
Nguyễn Bình <lx9182@gmail.com>
2023-07-24 20:32:51 +0700

    update templates and file path

--- app/run.py
+++ app/run.py
@@ -28,11 +28,11 @@ def tokenize(text):
     return clean_tokens
 
 # load data
-engine = create_engine('sqlite:///data/DisasterResponse.db')
+engine = create_engine('sqlite:///../data/DisasterResponse.db')
 df = pd.read_sql_table('disaster_response', engine)
 
 # load model
-model = joblib.load("models/classifier.pkl")
+model = joblib.load("../models/classifier.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
